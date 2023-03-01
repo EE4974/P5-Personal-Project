158e3d83288880760aee7799175604b15099c448
EE4974 <118484472+EE4974@users.noreply.github.com>
2023-03-01 12:28:35 -0800

    Enemy Waves

---
+++ P5 Personal Project/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+   public Transform enemyPrefab;
+   public  float timeBetweenWaves = 5;
+   public float countdown = 4;
+   private int waveIndex = 0;
+   public Transform spawnPoint;
+   public Text waveCountdownText;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave ()
+    {
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        waveIndex++;
+    }
+
+    void SpawnEnemy ()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
